09e2ce77dada25db44b996331e7b918b71a17866
Yas Alves <yasminqalves04@gmail.com>
2026-06-26 09:30:53 -0300

    Fix: correção de bugs na lógica JavaScript;

--- script.js
+++ script.js
@@ -36,16 +36,19 @@ const perguntas = {
   medio: [
     {
       pergunta: "Qual criatura voadora Jake doma?",
+      imagem: "https://i.pinimg.com/1200x/00/98/95/00989529959591f726144b5de653a7a6.jpg",
       opcoes: ["Ikran", "Tulkun", "Thanator", "Direhorse"],
       correta: 0,
     },
     {
       pergunta: "Quem é a companheira de Jake?",
+      imagem: "https://i.pinimg.com/1200x/d3/dd/dc/d3dddc8b0f7c4cfddeda847d2c0ee8ee.jpg",
       opcoes: ["Kiri", "Neytiri", "Ronal", "Mo'at"],
       correta: 1,
     },
     {
       pergunta: "O que é Eywa?",
+      imagem: "https://i.pinimg.com/1200x/7e/d8/bf/7ed8bfc3ac130d71c52fcfa0f7374178.jpg",
       opcoes: [
         "Uma árvore",
         "Uma arma",
@@ -56,36 +59,43 @@ const perguntas = {
     },
     {
       pergunta: "Qual é o recurso mineral procurado pelos humanos?",
+      imagem: "https://preview.redd.it/james-cameron-didnt-come-up-with-the-name-unobtainium-for-v0-8sg66uc5koy51.jpg?auto=webp&s=9b986750917e0dc59a48b878c78a1f9f9c48737a",
       opcoes: ["Adamantium", "Unobtanium", "Vibranium", "Kryptonite"],
       correta: 1,
     },
     {
       pergunta: "Qual é o sobrenome de Jake?",
+      imagem: "https://i.pinimg.com/736x/07/d9/49/07d949dfc601e45daea0818d8a8ba688.jpg",
       opcoes: ["Smith", "Cameron", "Sully", "Parker"],
       correta: 2,
     },
     {
       pergunta: "Quem lidera os militares humanos?",
+      imagem: "https://i.pinimg.com/1200x/25/e6/0f/25e60fcc03e9111a7dbce5ba9027256f.jpg",
       opcoes: ["Parker", "Quaritch", "Norm", "Max"],
       correta: 1,
     },
     {
       pergunta: "Avatar foi lançado em que ano?",
+      imagem: "https://i.pinimg.com/736x/06/5b/57/065b57a53178e887897400ec28c2e929.jpg",
       opcoes: ["2005", "2007", "2009", "2011"],
       correta: 2,
     },
     {
       pergunta: "Qual é a altura média dos Na'vi?",
+      imagem: "https://i.pinimg.com/1200x/e7/13/42/e71342ffa675185eb0c1a69dfaf1ca80.jpg",
       opcoes: ["2 metros", "3 metros", "4 metros", "5 metros"],
       correta: 1,
     },
     {
       pergunta: "Quem é a mãe de Neytiri?",
+      imagem: "https://i.pinimg.com/1200x/10/5c/e7/105ce7a118e9f945d93669ac49d604bc.jpg",
       opcoes: ["Ronal", "Kiri", "Mo'at", "Tsireya"],
       correta: 2,
     },
     {
       pergunta: "Avatar: O Caminho da Água foi lançado em?",
+      imagem: "https://i.pinimg.com/736x/c9/0d/64/c90d643037ff4bc98c1afdfbe32ccd1d.jpg",
       opcoes: ["2020", "2021", "2022", "2023"],
       correta: 2,
     },
@@ -94,16 +104,19 @@ const perguntas = {
   dificil: [
     {
       pergunta: "Qual é o nome da tribo de Neytiri?",
+      imagem: "https://i.pinimg.com/1200x/92/5b/c3/925bc34ab36d0c446fefcb0d5de18131.jpg",
       opcoes: ["Metkayina", "Omatikaya", "Ash Clan", "Mangkwan"],
       correta: 1,
     },
     {
       pergunta: "Quem interpreta Jake Sully?",
+      imagem: "https://i.pinimg.com/1200x/95/26/98/9526983901c3955f563c1db808dd1489.jpg",
       opcoes: ["Sam Worthington", "Tom Holland", "Chris Pratt", "Matt Damon"],
       correta: 0,
     },
     {
       pergunta: "Quem interpreta Neytiri?",
+      imagem: "https://i.pinimg.com/1200x/3d/8e/a9/3d8ea98dec3bfe0c9cf740d69ac111d4.jpg",
       opcoes: [
         "Scarlett Johansson",
         "Sigourney Weaver",
@@ -114,26 +127,31 @@ const perguntas = {
     },
     {
       pergunta: "Qual criatura é considerada um dos predadores mais perigosos?",
+      imagem: "https://i.pinimg.com/1200x/e0/d8/71/e0d8713fa6deb039fc66e348baac5954.jpg",
       opcoes: ["Tulkun", "Thanator", "Ikran", "Prolemuris"],
       correta: 1,
     },
     {
       pergunta: "Quem interpreta o Coronel Quaritch?",
+      imagem: "https://i.pinimg.com/1200x/2b/b3/45/2bb345b91d2078b24274c551ba4e6ca3.jpg",
       opcoes: ["Stephen Lang", "Vin Diesel", "Jason Momoa", "Josh Brolin"],
       correta: 0,
     },
     {
       pergunta: "Qual é o nome da filha adotiva de Jake?",
+      imagem: "https://i.pinimg.com/1200x/f3/0e/e3/f30ee3e0c56097617dbf3510c43898c5.jpg",
       opcoes: ["Tsireya", "Kiri", "Ronal", "Tuktirey"],
       correta: 1,
     },
     {
       pergunta: "Qual tribo vive nos oceanos?",
+      imagem: "https://i.pinimg.com/1200x/23/77/6e/23776e430348d29de0059ebec6c4c96e.jpg",
       opcoes: ["Omatikaya", "Metkayina", "Tayrangi", "Mangkwan"],
       correta: 1,
     },
     {
       pergunta: "Quem escreveu Avatar além de dirigir?",
+      imagem: "https://i.pinimg.com/1200x/d9/cf/50/d9cf502c506a34513350a5d971269f5f.jpg",
       opcoes: [
         "James Cameron",
         "George Lucas",
@@ -144,6 +162,7 @@ const perguntas = {
     },
     {
       pergunta: "Qual é o nome da árvore sagrada dos Na'vi?",
+      imagem: "https://i.pinimg.com/736x/17/fb/13/17fb138114e0c89c4f2ae38415704464.jpg",
       opcoes: [
         "Árvore das Vozes",
         "Árvore das Almas",
@@ -153,17 +172,20 @@ const perguntas = {
       correta: 1,
     },
     {
-      pergunta: "Qual filha de Jake aprende mais com os Tulkun?",
-      opcoes: ["Kiri", "Tuktirey", "Neytiri", "Ronal"],
+      pergunta: "Do que Jake era chamado após domar uma das mais perigosas montarias?",
+      imagem: "https://i.pinimg.com/1200x/93/ad/16/93ad16e24e7a1a5f8d36d2a245459bd5.jpg",
+      opcoes: ["Toruk Makto", "Thanator Makto", "Tulkun Rider", "Palulukan Rider"],
       correta: 0,
     },
     {
       pergunta: "Qual é a profissão de Grace Augustine?",
+      imagem: "https://i.pinimg.com/736x/5d/43/50/5d43504ec27a7c1afbbede2f68140d85.jpg",
       opcoes: ["Militar", "Pilota", "Cientista", "Médica"],
       correta: 2,
     },
     {
       pergunta: "Quem interpreta Grace Augustine?",
+      imagem: "https://i.pinimg.com/736x/1f/bc/f8/1fbcf8a94dbf0108bdd88f0dcb5c0322.jpg",
       opcoes: [
         "Zoe Saldaña",
         "Kate Winslet",
@@ -174,16 +196,19 @@ const perguntas = {
     },
     {
       pergunta: "Qual empresa explora Pandora?",
+      imagem: "https://i.pinimg.com/1200x/3a/36/06/3a3606b04b04ab5b843bbb857c744c4d.jpg",
       opcoes: ["RDA", "NASA", "SpaceX", "Weyland"],
       correta: 0,
     },
     {
       pergunta: "Qual é o nome do povo marinho aliado de Jake?",
+      imagem: "https://i.pinimg.com/736x/7d/43/35/7d4335ded5857852fa54cc57c243b6ca.jpg",
       opcoes: ["Metkayina", "Omatikaya", "Ash Clan", "Tawkami"],
       correta: 0,
     },
     {
       pergunta: "Qual elemento conecta biologicamente os seres de Pandora?",
+      imagem: "https://i.pinimg.com/1200x/21/a9/c2/21a9c297f5a3e64bc5daae203e485fa4.jpg",
       opcoes: ["DNA Quântico", "Rede Neural Biológica", "Magia", "Cristais"],
       correta: 1,
     },
@@ -193,6 +218,7 @@ let nivelEscolhido;
 let indiceAtual = 0;
 let pontos = 0;
 let perguntasAtuais = [];
+
 function IniciarQuiz() {
   const nivel = document.querySelector('input[name="nivel"]:checked');
 
@@ -209,61 +235,8 @@ function IniciarQuiz() {
   pontos = 0;
 
   mostrarPergunta();
-}function IniciarQuiz() {
-
-    const nivel = document.querySelector(
-        'input[name="nivel"]:checked'
-    );
-
-    if (!nivel) {
-        alert("Escolha um nível!");
-        return;
-    }
-
-    nivelEscolhido = nivel.value;
-
-    perguntasAtuais = perguntas[nivelEscolhido];
-
-    indiceAtual = 0;
-    pontos = 0;
-
-    mostrarPergunta();
-}function IniciarQuiz() {
-
-    const nivel = document.querySelector(
-        'input[name="nivel"]:checked'
-    );
-
-    if (!nivel) {
-        alert("Escolha um nível!");
-        return;
-    }
-
-    nivelEscolhido = nivel.value;
-
-    perguntasAtuais = perguntas[nivelEscolhido];
-
-    indiceAtual = 0;
-    pontos = 0;
-
-    mostrarPergunta();
-}function IniciarQuiz() {
-  const nivel = document.querySelector('input[name="nivel"]:checked');
-
-  if (!nivel) {
-    alert("Escolha um nível!");
-    return;
-  }
-
-  nivelEscolhido = nivel.value;
-
-  perguntasAtuais = perguntas[nivelEscolhido];
-
-  indiceAtual = 0;
-  pontos = 0;
-
-  mostrarPergunta();
 }
+
 
 function mostrarPergunta() {
   const container = document.getElementById("container");
@@ -287,7 +260,7 @@ function mostrarPergunta() {
               .join("")}
         </div>
 
-        <p>
+        <p class= "contador">
             Pergunta ${indiceAtual + 1}
             de ${perguntasAtuais.length}
         </p>
